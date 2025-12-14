72a5b8e5cee3ffcde4c29532fb081248b318799f
Ang Panha <angpanha2019@gmail.com>
2025-12-14 19:12:08 +0700

    chore: initial spring boot setup with H2, dependencies, package structure

---
+++ src/main/java/edu/itc/enrollment_scheduling_system/EnrollmentSchedulingSystemApplication.java
@@ -0,0 +1,13 @@
+package edu.itc.enrollment_scheduling_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnrollmentSchedulingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnrollmentSchedulingSystemApplication.class, args);
+	}
+
+}
