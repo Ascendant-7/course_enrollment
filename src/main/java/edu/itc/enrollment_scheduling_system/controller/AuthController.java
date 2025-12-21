a98d51a561f1dc7db2a045f41892cee58fe846ac
Anchhy <e20220608@dtc.itc.edu.kh>
2025-12-21 09:08:03 +0700

    Fix package structure: move controllers/dtos, move DBUserDetailsService, restore main class

--- src/main/java/edu/itc/enrollment_scheduling_system/controller/AuthController.java
+++ src/main/java/edu/itc/enrollment_scheduling_system/controller/AuthController.java
@@ -1,13 +1,12 @@
-package edu.itc.enrollment_scheduling_system.web;
 
+package edu.itc.enrollment_scheduling_system.controller;
 import edu.itc.enrollment_scheduling_system.service.RegistrationService;
-import edu.itc.enrollment_scheduling_system.web.dto.RegisterForm;
+import edu.itc.enrollment_scheduling_system.dto.RegisterForm;
 import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
-
 @Controller
 public class AuthController {
 
