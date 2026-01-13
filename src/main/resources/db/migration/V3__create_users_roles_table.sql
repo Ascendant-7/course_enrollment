3bd1a71b900738a5b07c21aaeb2b47c40ff1b493
MI-jerng <chimoengiphone@gmail.com>
2026-01-13 16:12:13 +0700

    create attendance_table for sql

--- src/main/resources/db/migration/V3__create_users_roles_table.sql
+++ src/main/resources/db/migration/V3__create_users_roles_table.sql
@@ -1,3 +1,4 @@
+-- Active: 1761750070668@@127.0.0.1@3306@course_enrollment
 CREATE TABLE users_roles (
     user_id BIGINT NOT NULL,
     role_id INT NOT NULL,
--- src/main/resources/db/migration/V5__create_enrollments.table.sql
+++ src/main/resources/db/migration/V5__create_enrollments.table.sql
@@ -1,9 +1,10 @@
+-- Active: 1761750070668@@127.0.0.1@3306@course_enrollment
 create table enrollments (
     id BIGINT PRIMARY KEY AUTO_INCREMENT,
     student_id BIGINT NOT NULL,
     course_id INT NOT NULL,
     created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-    CONSTRAINT fk_enrollment_student FOREIGN KEY (student_id) REFERENCES students (id)
-    CONSTRAINT fk_enrollment_student FOREIGN KEY (course_id) REFERENCES courses (id)
+    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT fk_enrollment_student FOREIGN KEY (student_id) REFERENCES students (id),
+    CONSTRAINT fk_enrollment_course FOREIGN KEY (course_id) REFERENCES courses (id)
 )
